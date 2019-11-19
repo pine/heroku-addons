d47e1e156dbbe0ca27578b5f8e83ff28ab00725f
Pine Mizune <pinemz@gmail.com>
2019-11-19 17:44:30 +0900

    feat: support JawsDB Maria

---
+++ src/main/java/moe/pine/heroku/addons/JawsDBMaria.java
@@ -0,0 +1,67 @@
+package moe.pine.heroku.addons;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Objects;
+
+public final class JawsDBMaria {
+    private static final @Nullable JawsDBMaria INSTANCE;
+
+    private final @NonNull String host;
+    private final @Nullable String username;
+    private final @Nullable String password;
+    private final @Nullable String database;
+    private final int port;
+
+    static {
+        final String mariaUrl = System.getenv("JAWSDB_MARIA_URL");
+        final MySQLUrlParser.Result result = MySQLUrlParser.parse(mariaUrl);
+        if (result != null) {
+            INSTANCE = new JawsDBMaria(result);
+        } else {
+            INSTANCE = null;
+        }
+    }
+
+    JawsDBMaria(final MySQLUrlParser.Result result) {
+        host = Objects.requireNonNull(result.host);
+        username = result.username;
+        password = result.password;
+        database = result.database;
+        port = result.port;
+    }
+
+    public static @Nullable JawsDBMaria get() {
+        return INSTANCE;
+    }
+
+    public @NonNull String getHost() {
+        return host;
+    }
+
+    public @NonNull String getUsername() {
+        if (username == null) {
+            throw new IllegalStateException("JawsDB Maria should have `username`. but not.");
+        }
+        return username;
+    }
+
+    public @NonNull String getPassword() {
+        if (password == null) {
+            throw new IllegalStateException("JawsDB Maria should have `password`, but not.");
+        }
+        return password;
+    }
+
+    public @NonNull String getDatabase() {
+        if (database == null) {
+            throw new IllegalStateException("JawsDB Maria should have `database`, but not.");
+        }
+        return database;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
